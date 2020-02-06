60c0033bfa5480710e05b13b59bdc6fb6c5a31e3
QuaintShanty <kristopher.ali.dev@gmail.com>
2020-02-06 09:52:53 -0500

    Started implementing the Cluster's structure.

--- src/Cluster/Cluster.cs
+++ src/Cluster/Cluster.cs
@@ -15,13 +15,85 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Sustenet.Cluster
 {
-    class Cluster
+    using System.Collections.Generic;
+    using System.Net.Sockets;
+    using TransportLayer;
+
+    class Cluster : Server
     {
+        public struct ClientData
+        {
+            string _name;
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            public ClientData(string name)
+            {
+                _name = name;
+            }
+        }
+
+        public Dictionary<string, ClientData> clients = new Dictionary<string, ClientData>();
+
+        /// <summary>
+        /// Creates a Transport Layer and prepares other functions.
+        /// </summary>
+        public Cluster() : base()
+        {
+
+        }
+
+        protected override void Init()
+        {
+            TransportLayerResponse responses = new TransportLayerResponse
+            {
+                OnListening = OnListening,
+
+                OnConnect = OnConnect,
+                OnDisconnect = OnDisconnect,
+
+                OnMessageSent = OnMessageSent,
+                OnMessageReceived = OnMessageReceived,
+
+                OnShutdown = OnShutdown
+            };
+
+            transport.Listen(responses);
+        }
+
+        void OnListening(Socket handler)
+        {
+
+        }
+
+        void OnConnect()
+        {
+
+        }
+
+        void OnDisconnect()
+        {
+
+        }
+
+        void OnMessageSent()
+        {
+
+        }
+
+        void OnMessageReceived()
+        {
+
+        }
+
+        void OnShutdown()
+        {
+
+        }
     }
 }
