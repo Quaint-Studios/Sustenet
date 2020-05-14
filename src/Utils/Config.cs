a3e29842e5a883ff585a8aed3b134ace051e0075
Makosai <kristopher.ali.dev@gmail.com>
2020-05-14 04:41:54 -0400

    Created a Utility namespace to house the Config class.

---
+++ src/Utils/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Sustenet.Utils
+{
+    /// <summary>
+    /// Loads confurations.
+    /// </summary>
+    class Config
+    {
+        public static NameValueConfigurationCollection GetConfig(string section)
+        {
+            var config = ConfigurationManager.GetSection("MasterServer") as NameValueConfigurationCollection;
+            Console.WriteLine(config);
+            return config;
+        }
+    }
+}
