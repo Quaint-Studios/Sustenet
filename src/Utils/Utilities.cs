f48e8af9c2e26237ff11d4a1620984df29d95bf8
Makosai <kristopher.ali.dev@gmail.com>
2020-05-14 13:35:02 -0400

    Created Nullable TryParse methods inside of the Utilities class.

---
+++ src/Utils/Utilities.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sustenet.Utils
+{
+    public static class Utilities
+    {
+        #region TryParseNullable
+        public static bool TryParseNullable(string n, out ushort? outVal)
+        {
+            ushort val;
+            if(ushort.TryParse(n, out val))
+            {
+                outVal = val;
+                return true;
+            }
+            else
+            {
+                outVal = null;
+                return false;
+            }
+        }
+        public static bool TryParseNullable(string n, out int? outVal)
+        {
+            int val;
+            if(int.TryParse(n, out val))
+            {
+                outVal = val;
+                return true;
+            }
+            else
+            {
+                outVal = null;
+                return false;
+            }
+        }
+        #endregion
+    }
+}
