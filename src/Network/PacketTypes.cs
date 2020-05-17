02ed701fe7f16b58a1409607bd5878b9ee5b286d
Makosai <kristopher.ali.dev@gmail.com>
2020-05-17 00:32:34 -0400

    Created PacketTypes to have a list of possible commands that can be handled.

---
+++ src/Network/PacketTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sustenet.Network
+{
+    public enum ServerPackets
+    {
+        welcome = 1
+    }
+
+    public enum ClientPackets
+    {
+        welcomeReceived = 1
+    }
+}
