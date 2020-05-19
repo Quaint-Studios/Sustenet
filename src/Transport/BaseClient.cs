db75edca01b3033fac0196da674495bea652d2a2
Makosai <kristopher.ali.dev@gmail.com>
2020-05-19 04:46:44 -0400

    Started debugging an issue with the DLL not running the ConnectCallback from BeginConnect() in the BaseClient.

--- src/Transport/BaseClient.cs
+++ src/Transport/BaseClient.cs
@@ -29,6 +29,13 @@ namespace Sustenet.Transport
         public TcpHandler tcp;
         public static int bufferSize = 4096;
 
+        public class Dbug
+        {
+            public int constructed, tcpconstructed, connected, received, senddata, debugClient, receivedCB, connectedCB, initialized = 0;
+        }
+
+        public static Dbug dbug = new Dbug();
+
         public BaseClient(int _id, bool debug = true)
         {
             id = _id;
@@ -37,6 +44,7 @@ namespace Sustenet.Transport
             if(debug)
                 tcp.onDebug.Run += (msg) => DebugClient(msg);
 
+            dbug.constructed++;
         }
 
         public class TcpHandler
@@ -49,15 +57,20 @@ namespace Sustenet.Transport
 
             public BaseEvent onConnected = new BaseEvent();
             public BaseEvent<byte[]> onReceived = new BaseEvent<byte[]>();
+            public BaseEvent<string> onDebug = new BaseEvent<string>();
 
             public TcpHandler(int _id)
             {
                 id = _id;
+
+                dbug.tcpconstructed++;
             }
 
             #region Connection Functions
             public void Receive(TcpClient _socket)
             {
+                dbug.received++;
+
                 if(socket != null)
                 {
                     if(stream != null)
@@ -82,11 +95,15 @@ namespace Sustenet.Transport
                     receiveBuffer = new byte[bufferSize];
                 }
 
-                stream.BeginRead(receiveBuffer, 0, bufferSize, ReceiveCallback, null);
+                stream.BeginRead(receiveBuffer, 0, bufferSize, new AsyncCallback(ReceiveCallback), null);
             }
 
-            private void ReceiveCallback(IAsyncResult ar)
+            public void ReceiveCallback(IAsyncResult ar)
             {
+                dbug.receivedCB++;
+
+                onDebug.RaiseEvent("RECEIVED DATA");
+
                 try
                 {
                     int byteLength = stream.EndRead(ar);
@@ -102,16 +119,18 @@ namespace Sustenet.Transport
 
                     onReceived.RaiseEvent(data);
 
-                    stream.BeginRead(receiveBuffer, 0, bufferSize, ReceiveCallback, null);
+                    stream.BeginRead(receiveBuffer, 0, bufferSize, new AsyncCallback(ReceiveCallback), null);
                 }
                 catch(Exception e)
                 {
-                    DebugClient($"Error with receiving TCP data...: {e}");
+                    onDebug.RaiseEvent($"Error with receiving TCP data...: {e}");
                 }
             }
 
             public void Connect(IPAddress ip, ushort port)
             {
+                dbug.connected++;
+
                 if(socket == null)
                 {
                     socket = new TcpClient
@@ -126,22 +145,24 @@ namespace Sustenet.Transport
                     receiveBuffer = new byte[bufferSize];
                 }
 
-                socket.BeginConnect(ip, port, ConnectCallback, null);
+                socket.BeginConnect(ip, port, new AsyncCallback(ConnectCallback), null);
             }
 
-            private void ConnectCallback(IAsyncResult ar)
+            public void ConnectCallback(IAsyncResult ar)
             {
+                dbug.connectedCB++;
+
                 try
                 {
                     socket.EndConnect(ar);
 
                     if(!socket.Connected)
                     {
-                        DebugClient($"Failed to connect to the server at {socket.Client.RemoteEndPoint}.");
+                        onDebug.RaiseEvent($"Failed to connect to the server at {socket.Client.RemoteEndPoint}.");
                         return;
                     }
 
-                    DebugClient($"Connected to server at {socket.Client.RemoteEndPoint}.");
+                    onDebug.RaiseEvent($"Connected to server at {socket.Client.RemoteEndPoint}.");
 
                     if(stream == null)
                     {
@@ -150,11 +171,11 @@ namespace Sustenet.Transport
 
                     onConnected.RaiseEvent();
 
-                    stream.BeginRead(receiveBuffer, 0, bufferSize, ReceiveCallback, null);
+                    stream.BeginRead(receiveBuffer, 0, bufferSize, new AsyncCallback(ReceiveCallback), null);
                 }
                 catch
                 {
-                    DebugClient("Error while trying to connect.");
+                    onDebug.RaiseEvent("Error while trying to connect.");
                 }
             }
             #endregion
@@ -162,6 +183,8 @@ namespace Sustenet.Transport
             #region Data Functions
             public void SendData(Packet packet)
             {
+                dbug.senddata++;
+
                 try
                 {
                     if(socket == null)
@@ -173,14 +196,15 @@ namespace Sustenet.Transport
                 }
                 catch(Exception e)
                 {
-                    DebugClient($"Error sending data via TCP...: {e}");
+                    onDebug.RaiseEvent($"Error sending data via TCP to Client#{id}...: {e}");
                 }
             }
             #endregion
         }
 
-        protected static void DebugClient(string msg)
+        private static void DebugClient(string msg)
         {
+            dbug.debugClient++;
             Console.WriteLine($"Client: {msg}");
         }
     }
