b8785e2367552173e1669834cb5d27e09b804184
Makosai <kristopher.ali.dev@gmail.com>
2020-05-24 14:52:15 -0400

    Loaded public keys on the MasterServer.

--- src/Master/MasterServer.cs
+++ src/Master/MasterServer.cs
@@ -38,7 +38,7 @@ namespace Sustenet.Master
 
         public MasterServer(int _maxConnections = 0, ushort _port = 6256) : base(_maxConnections, _port)
         {
-            Security.Keys.GetKey("TestName");
+            Security.Keys.LoadPubKeys();
 
             InitializeData();
 
