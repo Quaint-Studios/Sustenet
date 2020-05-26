c51d16c6eda423599fdef213875c9c538c78a4e9
Makosai <kristopher.ali.dev@gmail.com>
2020-05-25 20:08:16 -0400

    Created constants.

---
+++ src/Utils/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sustenet.Utils
+{
+    class Constants
+    {
+        /// <summary>
+        /// How many ticks are in a second.
+        /// </summary>
+        public const int TICK_RATE = 30;
+        public const int MS_PER_TICK = 1000 / TICK_RATE;
+    }
+}
