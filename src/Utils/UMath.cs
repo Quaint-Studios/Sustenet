98dfb8f34d23e9d8d2fd8e27a6f1d584ecb21301
Makosai <kristopher.ali.dev@gmail.com>
2020-06-25 05:47:55 -0400

    Added a new Utility class for math methods.

---
+++ src/Utils/UMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sustenet.Utils.Mathematics
+{
+    /// <summary>
+    /// The Math Utility class.
+    /// </summary>
+    public static class UMath
+    {
+        #region InRange
+        // TODO: Expand on this, adding more overloads and types.
+
+        /// <summary>
+        /// Determines if a number is in range of min and max (inclusive).
+        /// </summary>
+        public static bool InRange(this int num, int min, int max)
+        {
+            return num >= min && num <= max;
+        }
+
+        /// <summary>
+        /// Determines if a number is in range of min and max (inclusive).
+        /// </summary>
+        public static bool InRange(this float num, float min, float max)
+        {
+            return num >= min && num <= max;
+        }
+        #endregion
+    }
+}
