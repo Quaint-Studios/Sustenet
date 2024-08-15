fb3f7bb91d53efd386cd61936103364a9fccd036
Makosai <kristopher.ali.dev@gmail.com>
2024-08-15 16:34:43 -0400

    Add constants.
    These are all of the constants from the old Sustenet in C#. The ability to toggle debugging, set the tick rate, and default ports.

---
+++ src/Utils/Constants.zig
@@ -0,0 +1,8 @@
+pub const DEBUGGING = true;
+
+/// How many ticks are in a second.
+pub const TICK_RATE: i32 = 30;
+pub const MS_PER_TICK = 1000 / TICK_RATE;
+
+pub const MASTER_PORT: i16 = 6256;
+pub const CLUSTER_PORT: i16 = 6257;
