47cfd02fb08b88f30b3bc2e62b20b76dd3b6cf4b
Makosai <kristopher.ali.dev@gmail.com>
2024-08-15 16:36:03 -0400

    Add the network and utils namespaces to sustenet.

--- src/sustenet.zig
+++ src/sustenet.zig
@@ -1,3 +1,5 @@
 pub const clients = @import("Clients/clients.zig");
 pub const events = @import("Events/events.zig");
+pub const network = @import("Network/network.zig");
 pub const transport = @import("Transport/transport.zig");
+pub const utils = @import("Utils/utils.zig");
