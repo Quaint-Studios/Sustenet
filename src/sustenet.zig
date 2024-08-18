75de26e3e556f35b1be467995f01df99a367840a
Makosai <kristopher.ali.dev@gmail.com>
2024-08-17 22:44:02 -0400

    Add custom zig_numerics llib to the primary namespace.

--- src/sustenet.zig
+++ src/sustenet.zig
@@ -4,6 +4,8 @@ pub const network = @import("Network/network.zig");
 pub const transport = @import("Transport/transport.zig");
 pub const utils = @import("Utils/utils.zig");
 
+pub const zig_numerics = @import("deps/zig_numerics/zig_numerics.zig");
+
 test {
     @import("std").testing.refAllDecls(@This());
 }
