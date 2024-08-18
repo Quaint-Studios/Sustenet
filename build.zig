a4a3e625b78f4a4b6a5d8333663c5b695f74722c
Makosai <kristopher.ali.dev@gmail.com>
2024-08-17 22:42:49 -0400

    Add Zig-Network to the dependencies and update build.zig.

--- build.zig
+++ build.zig
@@ -21,14 +21,6 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(exe);
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 
     // Tests
     const tests = b.addTest(.{
@@ -39,6 +31,29 @@ pub fn build(b: *std.Build) void {
         .test_runner = b.path("test_runner.zig"),
     });
 
+    // build.zig.zon modules
+    const DEPS: [1][]const u8 = .{
+        "network",
+        // "zig_numerics",
+    };
+
+    for (DEPS) |name| {
+        const dep = b.dependency(name, .{ .target = target, .optimize = optimize });
+        exe.root_module.addImport(name, dep.module(name));
+        tests.root_module.addImport(name, dep.module(name));
+    }
+
+    // exe
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // tests
     const run_tests = b.addRunArtifact(tests);
     run_tests.has_side_effects = true;
     const test_step = b.step("test", "Run tests");
