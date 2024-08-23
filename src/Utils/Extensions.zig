09374541d7befee0ed087a97dd0ba284ae451c3e
Makosai <kristopher.ali.dev@gmail.com>
2024-08-23 10:49:44 -0400

    Turn Action invoke into a pointer.

--- src/Utils/Extensions.zig
+++ src/Utils/Extensions.zig
@@ -1,9 +1,13 @@
 //! Extensions for various things you might find useful from other languages.
 
+pub const ActionNA = struct {
+    fart: i32,
+};
+
 //#region Action
 /// A Zig implmenetation of C#'s `Action`.
-const Action = struct {
-    invoke: fn () void,
+pub const Action = struct {
+    invoke: *fn () void,
 
     pub fn create(callable: fn () void) Action {
         return Action{ .invoke = callable };
@@ -11,8 +15,8 @@ const Action = struct {
 };
 
 /// A Zig implmenetation of C#'s `Action<T>`.
-const ActionT = struct {
-    invoke: fn (comptime T: type) void,
+pub const ActionT = struct {
+    invoke: *fn (comptime T: type) void,
 
     pub fn create(callable: fn (T: type) void) ActionT {
         return ActionT{ .invoke = callable };
@@ -20,8 +24,8 @@ const ActionT = struct {
 };
 
 /// A Zig implmenetation of C#'s `Action<T1, T2>`.
-const ActionT1T2 = struct {
-    invoke: fn (comptime T1: type, comptime T2: type) void,
+pub const ActionT1T2 = struct {
+    invoke: *fn (comptime T1: type, comptime T2: type) void,
 
     pub fn create(callable: fn (T1: type, T2: type) void) ActionT1T2 {
         return ActionT1T2{ .invoke = callable };
