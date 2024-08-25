f6d97a8cc0181f481ac4c55c213dd91e4ca26c5f
Makosai <kristopher.ali.dev@gmail.com>
2024-08-25 18:52:01 -0400

    Create a Callable function for testing.

--- src/Events/BaseEvent.zig
+++ src/Events/BaseEvent.zig
@@ -5,6 +5,71 @@ const std = @import("std");
 
 const ArrayList = std.ArrayList;
 
+//#region Callable WORK IN PROGRESS REGION
+/// A way to have closures in Zig.
+///
+/// ```zig
+/// /// TODO example
+/// ```
+// pub fn Callable(comptime funcT: type, argsT: type) type {
+//     return struct {
+//         func: funcT,
+//         args: argsT,
+
+//         const Self = @This();
+
+//         pub fn init(func: funcT, args: argsT) Self {
+//             return Self{ .func = func, .args = args };
+//         }
+
+//         pub fn call(self: *Self) void {
+//             const ArgsType = @TypeOf(self.args);
+//             const args_type_info = @typeInfo(ArgsType);
+//             if (args_type_info != .Struct) {
+//                 @compileError("expected tuple or struct argument, found " ++ @typeName(ArgsType));
+//             }
+
+//             const fields_info = args_type_info.Struct.fields;
+//             if (fields_info.len == 0) {
+//                 self.func();
+//             } else if (fields_info.len == 1) {
+//                 self.func(@field(self.args, fields_info[0].name));
+//             } else if (fields_info.len == 2) {
+//                 self.func(
+//                     @field(self.args, fields_info[0].name),
+//                     @field(self.args, fields_info[1].name),
+//                 );
+//             } else if (fields_info.len == 3) {
+//                 self.func(
+//                     @field(self.args, fields_info[0].name),
+//                     @field(self.args, fields_info[1].name),
+//                     @field(self.args, fields_info[2].name),
+//                 );
+//             } else {
+//                 @compileError("3 argument max are supported per Callable call");
+//             }
+//         }
+//     };
+// }
+
+pub fn Callable(comptime funcT: anytype, args: anytype) CallableWrapper(funcT, @TypeOf(args)) {
+    return .{ .func = funcT, .args = args };
+}
+
+fn CallableWrapper(comptime funcT: anytype, comptime Args: type) type {
+    return struct {
+        func: *const @TypeOf(funcT),
+        args: Args,
+
+        const Self = @This();
+
+        pub fn call(self: *Self) void {
+            @call(.auto, self.func, .{self.args});
+        }
+    };
+}
+//#endregion
+
 //#region Action
 /// A Zig implmenetation of C#'s `Action`.
 pub fn Event() type {
