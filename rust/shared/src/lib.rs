f3b0a527bc60154ee283b161d63791341935ae79
Makosai <kristopher.ali.dev@gmail.com>
2025-04-18 20:56:50 -0400

    Create a Plugin trait for the shared lib.

--- rust/shared/src/lib.rs
+++ rust/shared/src/lib.rs
@@ -1,3 +1,5 @@
+use tokio::sync::mpsc::Sender;
+
 pub mod config;
 pub mod logging;
 pub mod network;
@@ -7,3 +9,19 @@ pub mod utils;
 pub mod security;
 
 pub mod macros;
+
+pub trait Plugin: Send + Sync {
+    fn receive(
+        &self,
+        tx: Sender<Box<[u8]>>,
+        command: u8
+    ) -> std::pin::Pin<Box<dyn std::future::Future<Output = ()> + Send>>;
+}
+
+// TODO: Use a Result instead.
+// #[derive(Debug)]
+// pub enum PluginError {
+//     Other {
+//         msg: String,
+//     },
+// }
