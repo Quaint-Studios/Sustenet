098861fb476e107a042bfc5d2284f7d40b125e1e
Makosai <kristopher.ali.dev@gmail.com>
2025-04-30 14:50:09 -0400

    Update main.rs

--- rust/master/src/main.rs
+++ rust/master/src/main.rs
@@ -7,7 +7,7 @@ async fn main() {
     let mut shutdown_rx = match utils::shutdown_channel() {
         Ok(rx) => rx,
         Err(e) => {
-            println!("Error creating shutdown channel: {e}");
+            eprintln!("Error creating shutdown channel: {e}");
             return;
         }
     };
@@ -30,27 +30,3 @@ async fn main() {
         }
     }
 }
-
-// use sustenet_shared as shared;
-
-// use sustenet_master::{ LOGGER, start_with_config };
-
-// use tokio::select;
-
-// use shared::utils;
-
-// pub mod security;
-
-// #[tokio::main]
-// async fn main() {
-//     let mut shutdown_rx = utils::shutdown_channel().expect("Error creating shutdown channel.");
-
-//     select! {
-//         _ = shutdown_rx.recv() => {
-//             LOGGER.warning("Shutting down...");
-//         }
-//         _ = start_with_config() => {}
-//     }
-
-//     LOGGER.success("The Master Server has been shut down.");
-// }
